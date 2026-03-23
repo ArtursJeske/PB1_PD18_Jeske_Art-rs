c10e697dc5f18a8f8a2af3517f1cf016ae4bdf4b
Artūrs Jeske <arturs.jeske1@gmail.com>
2026-03-23 19:25:15 +0200

    Pievienoti visi faili

---
+++ kalkulators.py
@@ -0,0 +1,7 @@
+# kalkulators.py
+
+def saskaitit(a, b):
+    """
+    Saskaita divus skaitļus un atgriež rezultātu. 
+    """
+    return a + b
---
+++ test_kalkulators.py
@@ -0,0 +1,13 @@
+import unittest
+from kalkulators import saskaitit
+    
+class TestKalkulators(unittest.TestCase):
+
+    def test_saskaitit_pozitivus(self):
+        self.assertEqual(saskaitit(5, 6), 11)
+
+    def test_saskaitit_negativus(self):
+        self.assertEqual(saskaitit(-2, -11), -13)
+
+if __name__ == "__main__":  
+    unittest.main()
